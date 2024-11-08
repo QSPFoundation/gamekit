27f01a2df0cb87a830203b63113839d41831ce5b
Varg <va@pm.me>
2024-11-08 23:27:56 +0100

    Use fixed package name w/o any version

--- build_games_package.sh
+++ build_games_package.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-export GAMEKIT_VER="${1:-latest}"
-
 mkdir -p dist
 
 # Validation
@@ -17,7 +15,7 @@ REL_BUILD_DIR=./build/package
 echo "Copying contents"
 cp -r $REL_CONTENTS_DIR/. $REL_BUILD_DIR
 
-echo "Compressing the package $GAMEKIT_VER"
-(cd $REL_BUILD_DIR; zip -7 -r ./games_package-$GAMEKIT_VER.zip ./)
+echo "Compressing the package"
+(cd $REL_BUILD_DIR; zip -7 -r ./games_package.zip ./)
 
 mv $REL_BUILD_DIR/*.zip ./dist/
